ffd0326d9e3adb07393c8a1db6ef9783b44ac7d0
LeoSanchez89 <58442719+LeoSanchez89@users.noreply.github.com>
2020-03-17 17:54:02 -0400

    seed data added

--- seeds/001-cars.js
+++ seeds/001-cars.js
@@ -1,13 +1,15 @@
 
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {colName: 'rowValue1'},
-        {colName: 'rowValue2'},
-        {colName: 'rowValue3'}
-      ]);
-    });
+exports.seed = async function(knex) {
+  
+  const testData = [
+		{ VIN: "JH2L560SF0BT0WI3V", make: "Ford", model: "Mustang", mileage: "100076", transmission: "manual", title_status: "clean" },
+		{ VIN: "108FKKL3V56A90HI3", make: "Toyota", model: "Corolla", mileage: "59405", transmission: "automatic", title_status: "clean" },
+		{ VIN: "R003KL6HZ8QIT98B4", make: "Honda", model: "Civic", mileage: "8443", transmission: "automatic", title_status: "salvage" },
+  ];
+  
+
+  await knex('cars').truncate();
+
+  return knex('cars').insert(testData);
+   
 };
